6ceec1898df5975fa6038a90f05a5cb542bdce94
omeravci99 <omeravci99@gmail.com>
2023-12-24 23:37:41 +0300

    Adding Zed Ros Wrapper

--- ozurover-setup.sh
+++ ozurover-setup.sh
@@ -104,3 +104,11 @@ sudo ./zedsdk.zstd.run
 # Install ZED Python Package
 echo "Installing ZED Python Package..."
 sudo pip3 install pyzed
+
+# Clone Zed Ros Wrapper
+echo "Cloning Zed Ros Wrapper..."
+cd ~/catkin_ws/src
+git --recurse-submodules https://github.com/stereolabs/zed-ros-wrapper.git
+cd ..
+catkin_make
+rosdep update
